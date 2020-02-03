b5eba45a6826eb7a413a8201bb4782638d0a7161
Riptod <otoval15>
2020-02-03 21:17:32 +0200

    add movie impl

---
+++ src/main/java/mate/academy/cinema/exceptions/DataProcessingException.java
@@ -0,0 +1,7 @@
+package mate.academy.cinema.exceptions;
+
+public class DataProcessingException extends Exception {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
