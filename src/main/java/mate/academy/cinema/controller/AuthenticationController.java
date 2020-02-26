cbd4278088c2c86cad863bab5905d35428cf756e
Riptod <otoval15>
2020-02-26 13:49:25 +0200

    fix controller

--- src/main/java/mate/academy/cinema/controller/AuthenticationController.java
+++ src/main/java/mate/academy/cinema/controller/AuthenticationController.java
@@ -5,7 +5,6 @@ import javax.validation.Valid;
 
 import mate.academy.cinema.dto.UserLoginDto;
 import mate.academy.cinema.dto.UserRegistrationDto;
-import mate.academy.cinema.exceptions.DataProcessingException;
 import mate.academy.cinema.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,9 +20,6 @@ public class AuthenticationController {
     @PostMapping(value = "/register")
     public String registerUser(@RequestBody @Valid UserRegistrationDto requestDto)
             throws AuthenticationException {
-        if (!requestDto.getPassword().equals(requestDto.getRepeatPassword())) {
-            throw new DataProcessingException("Passwords do not match");
-        }
         authenticationService.register(requestDto.getEmail(), requestDto.getPassword());
         return "Registration successfully";
     }
